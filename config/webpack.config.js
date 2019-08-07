3ec37e24e070b7bd1b3180cd5cafd349b7c30e9e
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-08-08 00:38:32 +0100

    Show app metrics using muze from Prometheus
    If we deploy a prometheus compatible API e.g. Metric Store we can query
    it from Paasmin
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -18,6 +18,7 @@ let cfg = {
     main: ['./src/main.ts'],
     "assets/init": ['./src/frontend/javascript/init.js'],
     "assets/sankey": ['./src/frontend/javascript/sankey.js'],
+    "assets/metrics": ['./src/frontend/javascript/metrics.js'],
   },
 
   output: {
